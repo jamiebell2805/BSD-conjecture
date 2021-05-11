d48dcc71321e945ff9837a368d949da59872ad38
James Bell <james.bell.20@ucl.ac.uk>
2021-05-11 10:25:17 +0100

    defining negation and addition

--- src/elliptic_curve.lean
+++ src/elliptic_curve.lean
@@ -30,7 +30,46 @@ def neg_finite : finite_points E → finite_points E
 
 def neg : points E → points E
 | 0 := 0
-| (some P) := 
-  
+| (some P) := (some neg_finite P)
+  --line above doesn't seem to work
+
+def double : points E → points E
+| 0 := 0
+| (some P) :=
+let ⟨⟨x, y⟩, h⟩ := P in
+if h2 : y = 0 then 0 else
+  let d := 2*y in
+  let sd := (3*x^2+E.a) in
+  let td := y*d-sd*x in
+  let x₂dd := sd^2-2*x*d^2 in
+  let y₂ddd := sd*x₂dd+td*d^3 in
+  let y₂' := -y₂ddd/d^3 in
+  let x₂ := x₂dd/d^2 in
+  some ⟨⟨x₂, y₂'⟩, begin
+  sorry,
+  end⟩
+
+def add : points E → points E → points E
+| 0 P := P
+| P 0 := P
+| (some P) (some Q) :=
+let ⟨⟨x1, y1⟩, h1⟩ := P in
+let ⟨⟨x2, y2⟩, h2⟩ := Q in
+if hd : x1 = x2 then (if y1 = y2 then double E (some P) else 0) else 
+  let d := (x1 - x2) in
+  let sd := (y1 - y2) in
+  let td := y1*d-sd*x1 in
+  let x3dd := sd^2-(x1+x2)*d*d in
+  let y3ddd := sd*x3dd+td*d*d in
+  let x₃ := x3dd/d^2 in
+  let y₃ := -y3ddd/d^3 in
+  some ⟨⟨x₃, y₃⟩, begin
+    sorry,
+  end⟩
+
+
+
+
+
 
 end elliptic_curve
